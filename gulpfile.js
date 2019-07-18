a9fc44a6d0b137beadd01bde151fd012e9864688
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-07-18 19:06:37 -0400

    updating gulp and changing priority of wp_enqueue_script

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ const plumber      = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const gutil        = require('gulp-util');
 const rename       = require('gulp-rename');
-const minify       = require('gulp-minify-css');
 const map          = require('map-stream');
 const browserlist  = ['last 2 version', '> 1%'];
 
@@ -44,7 +43,7 @@ gulp.task('default', function() {
 	console.log('gulp wordpress-lang to compile the lsx-projects.pot, en_EN.po and en_EN.mo');
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', function (done) {
 	return gulp.src('assets/css/scss/*.scss')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -62,10 +61,11 @@ gulp.task('styles', function () {
 			casacade: true
 		}))
 		.pipe(sourcemaps.write('maps'))
-		.pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css')),
+		done();
 });
 
-gulp.task('styles-rtl', function () {
+gulp.task('styles-rtl', function (done) {
 	return gulp.src('assets/css/scss/*.scss')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -85,12 +85,15 @@ gulp.task('styles-rtl', function () {
 		.pipe(rename({
 			suffix: '-rtl'
 		}))
-		.pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css')),
+		done();
 });
 
-gulp.task('compile-css', ['styles', 'styles-rtl']);
+gulp.task('compile-css', gulp.series( ['styles', 'styles-rtl'], function(done) {
+	done();
+}));
 
-gulp.task('js', function() {
+gulp.task('js', function(done) {
 	return gulp.src('assets/js/src/lsx-projects.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -102,10 +105,11 @@ gulp.task('js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('lsx-projects.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('admin-js', function() {
+gulp.task('admin-js', function(done) {
 	return gulp.src('assets/js/src/lsx-projects-admin.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -117,10 +121,11 @@ gulp.task('admin-js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('lsx-projects-admin.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('scporder-js', function() {
+gulp.task('scporder-js', function(done) {
 	return gulp.src('assets/js/src/scporder.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -132,22 +137,29 @@ gulp.task('scporder-js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('scporder.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('compile-js', ['js', 'admin-js', 'scporder-js']);
+gulp.task('compile-js', gulp.series( ['js', 'admin-js', 'scporder-js'] , function(done) {
+	done();
+}));
 
-gulp.task('watch-css', function () {
-	return gulp.watch('assets/css/**/*.scss', ['compile-css']);
+gulp.task('watch-css', function (done) {
+	done();
+	return gulp.watch('assets/css/**/*.scss',  gulp.series('compile-js'));
 });
 
-gulp.task('watch-js', function () {
-	return gulp.watch('assets/js/src/**/*.js', ['compile-js']);
+gulp.task('watch-js', function (done) {
+	done();
+	return gulp.watch('assets/js/src/**/*.js', gulp.series('compile-js'));
 });
 
-gulp.task('watch', ['watch-css', 'watch-js']);
+gulp.task('watch', gulp.series( ['watch-css', 'watch-js'] , function(done) {
+	done();
+}));
 
-gulp.task('wordpress-pot', function() {
+gulp.task('wordpress-pot', function(done) {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
@@ -155,10 +167,11 @@ gulp.task('wordpress-pot', function() {
 			package: 'lsx-projects',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
-		.pipe(gulp.dest('languages/lsx-projects.pot'))
+		.pipe(gulp.dest('languages/lsx-projects.pot')),
+		done();
 });
 
-gulp.task('wordpress-po', function() {
+gulp.task('wordpress-po', function(done) {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
@@ -166,13 +179,16 @@ gulp.task('wordpress-po', function() {
 			package: 'lsx-projects',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
-		.pipe(gulp.dest('languages/en_EN.po'))
+		.pipe(gulp.dest('languages/en_EN.po')),
+		done();
 });
 
-gulp.task('wordpress-po-mo', ['wordpress-po'], function() {
+gulp.task('wordpress-po-mo', gulp.series( ['wordpress-po'], function() {
 	return gulp.src('languages/en_EN.po')
 		.pipe(gettext())
-		.pipe(gulp.dest('languages'))
-});
+		.pipe(gulp.dest('languages'));
+}));
 
-gulp.task('wordpress-lang', (['wordpress-pot', 'wordpress-po-mo']));
+gulp.task('wordpress-lang', gulp.series( ['wordpress-pot', 'wordpress-po-mo'] , function(done) {
+	done();
+}));
